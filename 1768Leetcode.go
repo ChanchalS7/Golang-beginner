95b5d286ef6d937865585a0bfe32019380840570
Chanchal Verma <96100316+ChanchalS7@users.noreply.github.com>
2023-12-13 00:32:57 +0530

    Create 1768Leetcode.go

---
+++ 1768Leetcode.go
@@ -0,0 +1,32 @@
+// You are given two strings word1 and word2. Merge the strings by adding letters in alternating order, starting with word1. If a string is longer than the other, append the additional letters onto the end of the merged string.
+
+// Return the merged string.
+
+ 
+
+// Example 1:
+
+// Input: word1 = "abc", word2 = "pqr"
+// Output: "apbqcr"
+// Explanation: The merged string will be merged as so:
+// word1:  a   b   c
+// word2:    p   q   r
+// merged: a p b q c r
+
+func mergeAlternately(word1 string, word2 string) string {
+ i:=0
+ s:=""
+ for{
+     if len(word1)<=i && len(word2)<=i {
+         break
+     }
+     if len(word1) >i {
+         s+=string(word1[i])
+     }
+     if len(word2) > i {
+         s+=string(word2[i])
+     }
+     i++    
+ }  
+return s
+}
